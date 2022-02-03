5a51c132cf56556dbc9ffca56364fe014a385a8d
Rahul R <91786240+Rahul250298@users.noreply.github.com>
2022-02-03 19:00:46 +0530

    Adding  more than 3 parameter

--- GenericClassTwo.cs
+++ GenericClassTwo.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-    internal class GenericClassTwo
+     /* In this Generic Class
+     * using 2 Types place holder
+     * T and E
+     * place Holder name can be AnyThing.
+     */
+    internal class GenericClassTwo<T, E>
     {
+        public T[] array;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericClass{T}"/> class.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        public GenericClassTwo(T[] array)
+        {
+            this.array = array;
+        }
+        public void ToPrint()
+        {
+            Console.WriteLine("Using Generic class to print Array Element");
+            foreach (var item in array)
+                Console.WriteLine("Element in Array: " + item);
+        }
+        //Method for 2nd Type PlaceHolder.
+        public static void Value(E item)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
+  
+  
