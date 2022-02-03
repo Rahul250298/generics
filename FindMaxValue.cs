8811d2de49cb39bd2ada0b2a45d652f4d02f18d8
Rahul R <91786240+Rahul250298@users.noreply.github.com>
2022-02-03 19:13:57 +0530

    Adding the double maxvalue

--- FindMaxValue.cs
+++ FindMaxValue.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +6,10 @@ using System.Threading.Tasks;
 
 namespace Generics
 {
-     internal class FindMaxValue
-    {
-        /*Method to check Max number out of Three number
-        * If any two or all numbers are same then return zero.
-        */
+       internal class FindMaxValue
+    { /*Method to check Max number out of Three number
+         * If any two or all numbers are same then return zero.
+         */
         public int FindMaxInteger(int fisrtNum, int secondNum, int thirdNum)
         {
             //check condition for max number using CompareTo method.
@@ -37,7 +36,35 @@ namespace Generics
                 return 0;
             }
         }
+
+        //Mehod for Double type data value
+        public double FindMaxDouble(double first, double second, double third)
+        {
+            //check condition for max number using CompareTo method.
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("Max number is First Number: " + first);
+                return first;
+            }
+
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine("Max number is Second Number: " + second);
+                return second;
+            }
+
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine("Max number is Third Number: " + third);
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("two or more numbers are same");
+                return 0;
+            }
+        }
     }
 }
-  
+     
 
