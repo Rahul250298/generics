4064ccb9471ac673549c97aa1ac25dc92ffc34a8
Rahul R <91786240+Rahul250298@users.noreply.github.com>
2022-02-03 18:56:22 +0530

    Adding the Refactor code1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace Generics
     {
         static void Main(string[] args)
         {
+                //Instanse variables.
+            int first = 20, second = 300, third = 40;
+            double f = 40.5, s = 60.6, t = 35.79;
+            string firstName = "VikasBenki", secondName = "KarthikBenki", thirdName = "ManojBenki";
+
+            //Creating object of FindMaxValueClass        
+            Console.WriteLine("Max Integer number is:{0} out of numbers : {1} , {2} , {3} ", new FindMaxValue<int>().FindMax(first, second, third), first, second, third + "\n");
+            Console.WriteLine("Max double number is:{0} out of numbers : {1} , {2} , {3} ", new FindMaxValue<double>().FindMax(f, s, t), f, s, t + "\n");
+            Console.WriteLine("Max String Value is:{0} out of values : {1} , {2} , {3} ", new FindMaxValue<string>().FindMax(firstName, secondName, thirdName), firstName, secondName, thirdName + "\n");
+            Console.ReadLine();
         }
     }
 }
